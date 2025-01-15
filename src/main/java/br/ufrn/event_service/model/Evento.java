b612938ead1824cd0e8dc5920cc3ddba4ba7442d
Joaquim Chianca <joaquimchianca@gmail.com>
2025-01-14 21:10:12 -0300

    feat: criação modelo de evento

---
+++ src/main/java/br/ufrn/event_service/model/Evento.java
@@ -0,0 +1,52 @@
+package br.ufrn.event_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "eventos")
+public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeEvento;
+
+    @Column(nullable = false)
+    private LocalDateTime dataInicio;
+
+    @Column(nullable = false)
+    private LocalDateTime dataFim;
+
+    @Column(nullable = false)
+    private Boolean podeInscrever;
+
+    @Column(nullable = false)
+    private Long qtdParticipantes;
+
+    @Column(nullable = false)
+    private Long maxParticipantes;
+
+    @PrePersist
+    public void prePersist() {
+        if (dataInicio == null) {
+            dataInicio = LocalDateTime.now();
+        }
+        if (dataFim == null) {
+            dataFim = LocalDateTime.now();
+        }
+        if (podeInscrever == null) {
+            podeInscrever = true;
+        }
+    }
+}
